ecd669383b5f349e86968ff9d0bfd606c6575122
aurora <8.c.webpage@gmail.com>
2022-08-23 23:42:09 +0530

    application of convenient menu for all links

--- scripts.js
+++ scripts.js
@@ -14,12 +14,13 @@ setInterval(() => {
   }
 }, 1000);
 
-/* Set the width of the side navigation to 250px */
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
 }
 
-/* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
 }
