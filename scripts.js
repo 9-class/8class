473348c128c3c2aab50b8e170f13968a212bb912
aurora <109873714+class-8@users.noreply.github.com>
2022-08-28 11:32:25 +0530

    Navbar update

--- scripts.js
+++ scripts.js
@@ -1,11 +1,9 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+/* Open */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("myNav").style.height = "100%";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+/* Close */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("myNav").style.height = "0%";
 }
