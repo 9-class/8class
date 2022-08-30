d8313740c70ef317ce1906bb0afc7232ea5b880a
aurora <109873714+class-8@users.noreply.github.com>
2022-08-30 21:27:40 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,28 +9,4 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
-/* Storing user's device details in a variable*/
-let details = navigator.userAgent;
-  
-/* Creating a regular expression 
-containing some mobile devices keywords 
-to search it in details string*/
-let regexp = /android|iphone|kindle|ipad/i;
 
-/* Using test() method to search regexp in details
-it returns boolean value*/
-let isMobileDevice = regexp.test(details);
-
-if (isMobileDevice) {
-    check = "mobile device";
-} else {
-    check = "not mobile device";
-}
-let cent = document.getElementById('centre')
-if (check == "mobile device") {
-  console.log(check)
-  cent.style.textAlign = "center";
-} else {
-  console.log(check)
-  cent.style.textAlign = "left";
-}
