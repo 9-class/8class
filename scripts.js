e824c64adfc9004052ee169988555d7c01d3d6be
aurora <8.c.webpage@gmail.com>
2022-08-30 18:55:02 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,13 +9,22 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
-let check = ''
-if (window.matchMedia("(max-width: 767px)").matches)
-{
-// The viewport is less than 768 pixels wide
-check = "mobile device";
+/* Storing user's device details in a variable*/
+let details = navigator.userAgent;
+  
+/* Creating a regular expression 
+containing some mobile devices keywords 
+to search it in details string*/
+let regexp = /android|iphone|kindle|ipad/i;
+
+/* Using test() method to search regexp in details
+it returns boolean value*/
+let isMobileDevice = regexp.test(details);
+
+if (isMobileDevice) {
+    check = "mobile device";
 } else {
-  check = "not a mobile device";
+    check = "not mobile device";
 }
 let cent = document.getElementById('centre')
 if (check == "mobile device") {
