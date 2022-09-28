be5ccb1c868d3c2d314e2dc6427779c49f631920
aurora <8.c.webpage@gmail.com>
2022-09-28 23:51:20 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -18,40 +18,40 @@ setInterval(() =>{
   let dt = new Date();
   let gt = dt.getDate();
   let ot = dt.getMonth() + 1;
-  if (gt = 15 && ot = 12) {
+  if (gt == 15 && ot == 12) {
     let marq = document.getElementById('wish');
     marq.innerHTML = "Its Adharsh's birthday, don't forgot to wish him!";
-  } else if (gt = 10 && ot = 10) {
+  } else if (gt == 10 && ot == 10) {
     marq.innerHTML = "Its Balaji's birthday, don't forgot to wish him!";
-  } else if (gt = 14 && ot = 12) {
+  } else if (gt == 14 && ot == 12) {
     marq.innerHTML = "Its Dhikshan's birthday, don't forgot to wish him!";
-  } else if (gt = 1 && ot = 10) {
+  } else if (gt == 1 && ot == 10) {
     marq.innerHTML = "Its Gokula Krishnan's birthday, don't forgot to wish him!";
-  } else if (gt = 18 && ot = 8) {
+  } else if (gt == 18 && ot == 8) {
     marq.innerHTML = "Its Harsha Varman's birthday, don't fotgot to wish him!";
-  } else if (gt = 3 && ot = 7) {
+  } else if (gt == 3 && ot == 7) {
     marq.innerHTML = "Its Joshuva's birthday, don't forgot to wish him!";
-  } else if (gt = 10 && ot = 10) {
+  } else if (gt == 10 && ot == 10) {
     marq.innerHTML = "Its Keerthivasan's birthday, don't forgot to wish him!";
-  } else if (gt = 20 && ot = 1) {
+  } else if (gt == 20 && ot == 1) {
     marq.innerHTML = "Its Mitran's birthday, don't forgot to wish him!";
-  } else if (gt = 21 && ot = 10) {
+  } else if (gt == 21 && ot == 10) {
     marq.innerHTML = "Its Nitin Kamal(co-founder)'s birthday, don't forgot to wish him!";
-  } else if (gt = 27 && ot = 7) {
+  } else if (gt == 27 && ot == 7) {
     marq.innerHTML = "Its Praveen's birthday, don't forgot to wish him";
-  } else if (gt = 26 && ot = 11) {
+  } else if (gt == 26 && ot == 11) {
     marq.innerHTML = "Its Sanjay's birthday, don't forgot to wish him!";
-  } else if (gt = 11 && ot = 12) {
+  } else if (gt == 11 && ot == 12) {
     marq.innerHTML = "Its Shaswad's birthday, don't forgot to wish him!";
-  } else if (gt = 26 && ot = 2) {
+  } else if (gt == 26 && ot == 2) {
     marq.innerHTML = "Its Seshavardhan's birthday, don't forgot to wish him!";
-  } else if (gt = 17 && ot = 2) {
+  } else if (gt == 17 && ot == 2) {
     marq.innerHTML = "Its Srivarshan's birthday, don't forgot to wish him!";
-  } else if (gt = 14 && ot = 3) {
+  } else if (gt == 14 && ot == 3) {
     marq.innerHTML = "Its Saichiranjeevi's birthday, don't forgot to wish him!";
-  } else if (gt = 5 && ot = 3) {
+  } else if (gt == 5 && ot == 3) {
     marq.innerHTML = "Its Tinakaran's birthday, don't forgot to wish him!";
-  } else if (gt = 2 && ot = 12) {
+  } else if (gt == 2 && ot == 12) {
     marq.innerHTML = "Its Vishnu Vikram(founder)'s birthday, don't forgot to wish him!";
   }
 },50)
